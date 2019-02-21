6a9cf15ad2bc4fc43c9424f235aa8ca83e6130ae
Zak Barlow <zak@stormideas.com>
2019-02-21 10:22:08 +0000

    Added helper methods for setting constraints - demo now working correctly

---
+++ CollectionViewInsteadOfPagerView/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExtensions.swift
+//  CollectionViewInsteadOfPagerView
+//
+//  Created by Zak Barlow on 21/02/2019.
+//  Copyright © 2019 zakbarlow. All rights reserved.
+//
+
+import Foundation
---
+++ CollectionViewInsteadOfPagerView/Extensions/UIViewExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExtensions.swift
+//  CollectionViewInsteadOfPagerView
+//
+//  Created by Zak Barlow on 21/02/2019.
+//  Copyright © 2019 zakbarlow. All rights reserved.
+//
+
+import Foundation
