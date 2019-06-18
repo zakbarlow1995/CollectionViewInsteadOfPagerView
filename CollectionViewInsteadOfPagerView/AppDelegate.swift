bb06cd9e9cd11f0d605c5cd78c93977b0f17e96f
zakbarlow1995 <zak@stormideas.com>
2019-06-18 15:55:56 +0100

    Removed storyboard, added navController w/title + refactored MainVC as UICollectionViewController

--- CollectionViewInsteadOfPagerView/AppDelegate.swift
+++ CollectionViewInsteadOfPagerView/AppDelegate.swift
@@ -13,9 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        let navController = UINavigationController(rootViewController: MainVC(collectionViewLayout: UICollectionViewFlowLayout()))
+        window?.rootViewController = navController
         return true
     }
 
--- CollectionViewInsteadOfPagerView/ViewControllers/MainVC.swift
+++ CollectionViewInsteadOfPagerView/ViewControllers/MainVC.swift
@@ -9,29 +9,26 @@
 import Foundation
 import UIKit
 
-class MainVC: UIViewController, UICollectionViewDelegateFlowLayout {
+class MainVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     fileprivate let menuController = MenuVC(collectionViewLayout: UICollectionViewFlowLayout())
     fileprivate let cellId = "cellId"
     
     fileprivate let pages = ["aVC", "bVC", "cVC"]
     
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 0
-        layout.scrollDirection = .horizontal
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .white
-        cv.showsVerticalScrollIndicator = false
-        cv.showsHorizontalScrollIndicator = false
-        return cv
-    }()
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "CollectionView \"Pager\" Demo"
+        
+        view.backgroundColor = .white
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+            layout.minimumLineSpacing = 0
+        }
+        
         menuController.delegate = self
+        menuController.collectionView.selectItem(at: [0, 0], animated: true, scrollPosition: .centeredHorizontally)
         
         setupLayout()
     }
@@ -45,8 +42,6 @@ class MainVC: UIViewController, UICollectionViewDelegateFlowLayout {
         collectionView.dataSource = self
         collectionView.delegate = self
         
-        view.backgroundColor = .white
-        
         menuView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: self.topBarTotalHeight, left: 0, bottom: 0, right: 0) , size: .init(width: view.bounds.width, height: 30))
         collectionView.anchor(top: menuView.bottomAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
         
@@ -56,9 +51,6 @@ class MainVC: UIViewController, UICollectionViewDelegateFlowLayout {
         collectionView.isPagingEnabled = true
         collectionView.bounces = false
         collectionView.allowsSelection = true
-        
-        menuController.collectionView.selectItem(at: [0, 0], animated: true, scrollPosition: .centeredHorizontally)
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -69,6 +61,40 @@ class MainVC: UIViewController, UICollectionViewDelegateFlowLayout {
     func setupNavigationBar() {
         setupTransparentNavigationBarWithBlackText()
     }
+
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let x = scrollView.contentOffset.x
+        let offset = x / CGFloat(pages.count)
+        menuController.menuBar.transform = CGAffineTransform(translationX: offset, y: 0)
+    }
+    
+    override func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        let item = Int(scrollView.contentOffset.x / view.frame.width)
+        let indexPath = IndexPath(item: item, section: 0)
+        collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .bottom)
+    }
+    
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let x = targetContentOffset.pointee.x
+        let item = Int(x / view.frame.width)
+        let indexPath = IndexPath(item: item, section: 0)
+        menuController.collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredHorizontally)
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MainCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: collectionView.bounds.height)
+    }
     
 }
 
@@ -77,44 +103,6 @@ extension MainVC: MenuVCDelegate {
     // Delegate method implementation (scroll to the right page when the corresponding Menu "Button"(Item) is pressed
     func didTapMenuItem(indexPath: IndexPath) {
         collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-    }
-    
-}
-
-extension MainVC: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let x = scrollView.contentOffset.x
-        let offset = x / CGFloat(pages.count)
-        menuController.menuBar.transform = CGAffineTransform(translationX: offset, y: 0)
-    }
-    
-    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-        let item = Int(scrollView.contentOffset.x / view.frame.width)
-        let indexPath = IndexPath(item: item, section: 0)
-        collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .bottom)
-    }
-    
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        let x = targetContentOffset.pointee.x
-        let item = Int(x / view.frame.width)
-        let indexPath = IndexPath(item: item, section: 0)
-        menuController.collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredHorizontally)
-    }
-    
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pages.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MainCell
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: collectionView.bounds.height)
     }
     
 }
